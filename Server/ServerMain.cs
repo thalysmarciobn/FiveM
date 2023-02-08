936f4f60f19e9387aa443154d3f8aa24cccd385f
Thalys Márcio <thalys@live.com>
2023-02-08 00:00:37 -0300

    Player Handler

--- Server/ServerMain.cs
+++ Server/ServerMain.cs
@@ -1,6 +1,11 @@
 using System;
+using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 using CitizenFX.Core;
+using Database;
+using Microsoft.EntityFrameworkCore;
+using Models.Database;
 
 namespace FiveM.Server
 {
@@ -14,7 +19,45 @@ namespace FiveM.Server
         [EventHandler("playerConnecting")]
         private void PlayerConnecting([FromSource] Player player, string playerName, dynamic kickCallback, dynamic deferrals)
         {
+            var license = player.Identifiers["license"];
 
+            deferrals.defer();
+
+            using (var context = new FiveMContext())
+            {
+                var account = context.Accounts.FirstOrDefault(x => x.License == license);
+
+                if (account == null)
+                {
+                    account = new AccountModel
+                    {
+                        License = license,
+                        Created = DateTime.Now,
+                        WhiteListed = true
+                    };
+                    context.Accounts.Add(account);
+                    context.SaveChanges();
+                }
+
+                deferrals.done();
+
+                Debug.WriteLine($"[{account.Id}] Account connected: {account.License}");
+            }
+        }
+
+        [EventHandler("playerDropped")]
+        private void OnPlayerDropped([FromSource] Player player, string reason)
+        {
+            var license = player.Identifiers["license"];
+
+            using (var context = new FiveMContext())
+            {
+                var account = context.Accounts.FirstOrDefault(x => x.License == license);
+                if (account != null)
+                {
+                    Debug.WriteLine($"[{account.Id}] Account {player.Name} dropped for reason: {reason}");
+                }
+            }
         }
 
         [Command("hello_server")]
