c3fc6ef330a96c147869c9794ed85ea86d8fc0ee
Thalys Márcio <thalys@live.com>
2023-02-09 11:48:45 -0300

    Render Character

--- Client/ClientMain.cs
+++ Client/ClientMain.cs
@@ -3,6 +3,8 @@ using System.Globalization;
 using System.IO;
 using System.Threading.Tasks;
 using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using Client.Extensions;
 using Newtonsoft.Json;
 using Shared.Models.Database;
 using static CitizenFX.Core.Native.API;
@@ -26,12 +28,46 @@ namespace FiveM.Client
             EventHandlers["ProjectCharacterData"] += new Action<string>(ProjectCharacterData);
         }
 
-        private void ProjectCharacterData(string json)
+        private async void ProjectCharacterData(string json)
         {
-            Debug.WriteLine("xxxxxxxxxxx");
-            Debug.WriteLine(json);
-            AccountCharacterModel jsonResponse = Serializer.Deserialize<AccountCharacterModel>(new JsonTextReader(new StringReader(json)));
-            Debug.WriteLine(jsonResponse.Name);
+            var player = Game.Player;
+
+            var character = Serializer.Deserialize<AccountCharacterModel>(new JsonTextReader(new StringReader(json)));
+
+            player.Spawn(new Vector3
+            {
+                X = character.Position.X,
+                Y = character.Position.Y,
+                Z = character.Position.Z
+            });
+
+            Game.PlayerPed.Heading = 226.2f;
+
+            while (!await Game.Player.ChangeModel(new Model(character.Model))) await Delay(10);
+
+            player.SetPedHeadBlendDatas(character.PedHeadData);
+            player.SetPedHead(character.PedHead);
+            player.SetPedHeadOverlays(character.PedHeadOverlay);
+            player.SetPedHeadOverlayColors(character.PedHeadOverlayColor);
+            player.SetPedFaceFeatures(character.PedFace);
+
+            player.Character.Armor = character.Armor;
+
+            player.StyleComponents(character.PedComponent);
+            player.StyleProps(character.PedProp);
+
+            Game.Player.Unfreeze();
+
+            SwitchInPlayer(PlayerPedId());
+
+            // API.RequestClipSet(character.WalkingStyle);
+            // await BaseScript.Delay(100);
+            // Game.Player.Character.MovementAnimationSet = this.WalkingStyle;
+
+
+
+            Debug.WriteLine(character.Name);
+
         }
 
         [Tick]
--- Client/Extensions/PlayerExtensions.cs
+++ Client/Extensions/PlayerExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
 using Shared.Models.Database;
 using System.Collections.Generic;
 using static CitizenFX.Core.Native.API;
@@ -9,33 +10,34 @@ namespace Client.Extensions
     {
         public static void SetPedHeadBlendDatas(this Player player, AccountCharacterPedHeadDataModel model)
         {
-            SetPedHeadBlendData(player.Handle, model.ShapeFirstID, model.ShapeSecondID, model.ShapeThirdID, model.SkinFirstID, model.SkinSecondID, model.SkinThirdID, model.ShapeMix, model.SkinMix, model.ThirdMix, model.IsParent);
+            var ped = player.Character.Handle;
+            SetPedHeadBlendData(ped, model.ShapeFirstID, model.ShapeSecondID, model.ShapeThirdID, model.SkinFirstID, model.SkinSecondID, model.SkinThirdID, model.ShapeMix, model.SkinMix, model.ThirdMix, model.IsParent);
         }
 
         public static void SetPedHead(this Player player, AccountCharacterPedHeadModel model)
         {
-            var ped = player.Handle;
+            var ped = player.Character.Handle;
             SetPedHairColor(ped, model.HairColorId, model.HairHighlightColor);
             SetPedEyeColor(ped, model.EyeColorId);
         }
 
         public static void SetPedHeadOverlays(this Player player, ICollection<AccountCharacterPedHeadOverlayModel> model)
         {
-            var ped = player.Handle;
+            var ped = player.Character.Handle;
             foreach (var overlay in model)
                 SetPedHeadOverlay(ped, (int)overlay.OverlayId, overlay.Index, overlay.Opacity);
         }
 
         public static void SetPedHeadOverlayColors(this Player player, ICollection<AccountCharacterPedHeadOverlayColorModel> model)
         {
-            var ped = player.Handle;
+            var ped = player.Character.Handle;
             foreach (var overlay in model)
                 SetPedHeadOverlayColor(ped, (int)overlay.OverlayId, overlay.ColorType, overlay.ColortId, overlay.SecondColorId);
         }
 
         public static void SetPedFaceFeatures(this Player player, ICollection<AccountCharacterPedFaceModel> shapes)
         {
-            var ped = player.Handle;
+            var ped = player.Character.Handle;
             foreach (var shape in shapes)
                 SetPedFaceFeature(ped, (int) shape.Index, shape.Scale);
         }
@@ -51,5 +53,35 @@ namespace Client.Extensions
             foreach (var component in components)
                 player.Character.Style[(PedProps)component.PropId].SetVariation(component.Index, component.Texture);
         }
+
+        public static void Freeze(this Player player, bool freeze = true)
+        {
+            player.CanControlCharacter = !freeze;
+            player.Character.IsVisible = !freeze;
+            player.Character.IsCollisionEnabled = !freeze;
+            player.Character.IsPositionFrozen = freeze;
+            player.Character.IsInvincible = freeze;
+            player.Character.Task.ClearAllImmediately();
+        }
+
+        public static void Unfreeze(this Player player) => player.Freeze(false);
+
+        public static void Spawn(this Player player, Vector3 position)
+        {
+            player.Freeze();
+
+            LoadScene(position.X, position.Y, position.Z);
+            RequestCollisionAtCoord(position.X, position.Y, position.Z);
+
+            player.Character.Style.SetDefaultClothes();
+
+            player.Character.Position = position;
+            player.Character.ClearBloodDamage();
+            player.Character.Weapons.Drop();
+            player.WantedLevel = 0;
+
+            player.Unfreeze();
+        }
+
     }
 }
